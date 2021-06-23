b68a8e293475df5b51698ef07d8e5d8ff6fd0166
Arya K <aryakiran@zohomail.eu>
2021-06-23 09:50:33 +0530

    Added my emacs configuration

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f59acbeacefb4998f45126d4d8ae8b2184f2a48753db362a349fd55321c7e1" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "6c386d159853b0ee6695b45e64f598ed45bd67c47f671f69100817d7db64724d" default))
+ '(user-full-name "Arya Kiran")
+ '(user-mail-address "k120223@psbbschools.edu.in"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(org-babel-load-file
+ (expand-file-name
+  "configuration.org"
+  user-emacs-directory))
