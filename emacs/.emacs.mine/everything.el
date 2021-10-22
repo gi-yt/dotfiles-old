bba741765746c2d828e863e685734f9ec4704372
Arya K <aryakiran@zohomail.eu>
2021-10-22 17:06:38 +0530

    Add VC binds

--- emacs/.emacs.mine/everything.el
+++ emacs/.emacs.mine/everything.el
@@ -23,7 +23,20 @@
   :config
   (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
   (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
-
+;; Git Keybindings With VC and Magit
+(general-define-key
+ :prefix "C-c g"
+ "g d" '(vc-root-diff :which-key "Give a diff for all modified files")
+ "l d" '(vc-diff :which-key "Give a diff for modifications in current file")
+ "d" 'vc-next-action
+ "l s" '(magit-stage :which-key "Stage current file if not staged")
+ "l l"   '(vc-print-log :which-key "Show log for current vc file")
+ "r l" '(vc-region-history :which-key "Show the vc history of the current region")
+ "g l"   '(magit-log-all :which-key "Show log for the whole project")
+ "p" '(vc-push :which-key "Push Commits to remote")
+ "s" '(magit-status :which-key "Show status")
+ "c" '(magit-commit :which-key "Commit") ;; Need a better way to commit with vc.el until then stickin to magit
+ "l a" '(vc-annotate :which-key "Show annotations"))
 ;;; Best Terminal Emulator For Emacs
 ;; Vterm can do alot of cool stuff
 (use-package vterm :straight t :defer t)
@@ -38,24 +51,24 @@
 ;;; Cool startpage
 (use-package page-break-lines)
 (use-package dashboard :after page-break-lines
-  :straight t
-  :init                                                                      ;; tweak dashboard config before loading it
-  (setq dashboard-set-heading-icons t)                                       ;; add icons for headings
-  (setq dashboard-set-file-icons t)                                          ;; add icons for files
-  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")     ;; set a title to be displayed under the banner
-  (setq dashboard-center-content nil)                                          ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)                                      ;; show some items on dashboard
-                          (bookmarks . 5)
-                          (registers . 5)))
-  (setq dashboard-page-separator "\n\f\n")    ;; <-----
-  (global-page-break-lines-mode)
-  :config
-  (dashboard-setup-startup-hook)
-  (dashboard-modify-heading-icons '((recents . "file-text")
-                                    (bookmarks . "book")))
-  :custom-face
-  (dashboard-items-face ((t (:inherit widget-button :weight normal))))
-  (dashboard-heading ((t (:inherit font-lock-keyword-face :weight semi-bold)))))
+             :straight t
+             :init                                                                      ;; tweak dashboard config before loading it
+             (setq dashboard-set-heading-icons t)                                       ;; add icons for headings
+             (setq dashboard-set-file-icons t)                                          ;; add icons for files
+             (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")     ;; set a title to be displayed under the banner
+             (setq dashboard-center-content nil)                                          ;; set to 't' for centered content
+             (setq dashboard-items '((recents . 5)                                      ;; show some items on dashboard
+                                     (bookmarks . 5)
+                                     (registers . 5)))
+             (setq dashboard-page-separator "\n\f\n")    ;; <-----
+             (global-page-break-lines-mode)
+             :config
+             (dashboard-setup-startup-hook)
+             (dashboard-modify-heading-icons '((recents . "file-text")
+                                               (bookmarks . "book")))
+             :custom-face
+             (dashboard-items-face ((t (:inherit widget-button :weight normal))))
+             (dashboard-heading ((t (:inherit font-lock-keyword-face :weight semi-bold)))))
 
 ;;; EAF
 ;; Do everything in emacs from browser to pdf reader to video player
@@ -78,4 +91,18 @@
 (use-package ctable :defer t)
 (use-package s :defer t)
 (use-package deferred :defer t)
+;;; Meme.el
+;; The Greatest meme generator
+(use-package imgur
+  :ensure t
+  :straight (imgur
+             :type git
+             :host github
+             :repo "myuhe/imgur.el"))
 
+(use-package meme
+  :ensure t
+  :straight (meme
+             :type git
+             :host github
+             :repo "larsmagne/meme"))
